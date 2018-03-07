afed03cb19161d726439a78cba0fdcab2c3211aa
Vikas Sharma <viksharm@adobe.com>
2018-03-07 19:30:58 +0530

    .

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -8,12 +8,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -22,8 +22,8 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        
-        alert('Hi' + camera + '!');
+
+        alert('Hi!');
     }
 };
 
